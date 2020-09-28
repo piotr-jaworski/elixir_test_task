863e8623279621beab8dcf6f91b5555abb70fd7c
Piotr <ppiotr.jaworski@gmail.com>
2020-09-28 11:33:47 +0200

    Turn off debug_errors

--- config/dev.exs
+++ config/dev.exs
@@ -17,7 +17,7 @@ config :credit, Credit.Repo,
 # with webpack to recompile .js and .css sources.
 config :credit, CreditWeb.Endpoint,
   http: [port: 4000],
-  debug_errors: true,
+  debug_errors: false,
   code_reloader: true,
   check_origin: false,
   watchers: [
